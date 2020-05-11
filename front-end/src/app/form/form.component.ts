d259a4909bc5cbb0a2d5430ec208ff2d0ea4cf00
BellatiS <bellati.samuele@iisgalvanimi.edu.it>
2020-05-11 16:49:30 +0000

    form1

--- front-end/src/app/form/form.component.ts
+++ front-end/src/app/form/form.component.ts
@@ -3,6 +3,7 @@ import {  //Importa FormBuilder e FormGroup
   FormBuilder,
   FormGroup
 } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -11,21 +12,26 @@ import {  //Importa FormBuilder e FormGroup
 
 export class FormComponent implements OnInit {
   myForm: FormGroup;  //Dichiara una variabile di tipo FormGroup
-  constructor(fb: FormBuilder) { //Il costruttore riceve come parametro il From Builder
+  constructor(fb: FormBuilder,private http:HttpClient) { //Il costruttore riceve come parametro il From Builder
 
     /*Diciamo al FormBuilder di creare un FormGroup che conterrà un FormControl
      *Chiamato sku, con valore di default ABC123 */
     this.myForm = fb.group({
-      'nome': ['ABC123'],
-      'colore': ['abc']
+      'id': ['123'],
+      'nome_comune': ['abc'],
+      'altezza': ['abc'],
+      'periodo': ['abc'],
+      'note': ['abc'],
+      'famiglia': ['abc'],
+      'scient': ['abc']
     });
   }
   ngOnInit() {
   }
   onSubmit(value: string): void {
     console.log('you submitted value: ', value);
+    let headers = {headers: new HttpHeaders().set('Content-Type', 'application/json')};
+    this.http.post('https://3000-a0a65d53-ab3e-4aab-97e1-0a738189b07d.ws-eu01.gitpod.io/users/inserisci',(value),headers);
+
   }
 }
-
-
-
