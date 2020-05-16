af88cf80e57bf30ee8dc9867d0ef1e9bb9f32759
Marco <bissoli.marco@iisgalvanimi.edu.it>
2020-05-16 13:17:02 +0000

    Il malizia non è capace

--- back-end/app.js
+++ back-end/app.js
@@ -14,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,7 +28,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+app.use(cors);
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
